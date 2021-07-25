1722ef25ddb0266bf2bc7f395be42c7bb9693739
ltogt <omnesiaorg@gmail.com>
2021-07-25 20:01:03 +0200

    add sized_box_2

--- lib/src/const/sized_box.dart
+++ lib/src/const/sized_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
+const SizedBox SIZED_BOX_2 = SizedBox(height: 2, width: 2);
 const SizedBox SIZED_BOX_5 = SizedBox(height: 5, width: 5);
 const SizedBox SIZED_BOX_10 = SizedBox(height: 10, width: 10);

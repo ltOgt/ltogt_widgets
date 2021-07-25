f089301e94949f51d379c50d596d48a842414cb0
ltogt <omnesiaorg@gmail.com>
2021-07-25 11:15:52 +0200

    add text field as test input with controller incl

---
+++ lib/src/brick/brick_text_field.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:ltogt_widgets/ltogt_widgets.dart';
+
+class BrickTextField extends StatefulWidget {
+  const BrickTextField({
+    Key? key,
+    required this.onChange,
+    required this.hint,
+    this.maxLines,
+    this.bgColor,
+    this.textColor,
+    this.fontSize,
+    this.editable = true,
+  }) : super(key: key);
+
+  final Function(String text) onChange;
+  final String hint;
+  final int? maxLines;
+  final Color? bgColor;
+  final Color? textColor;
+  final double? fontSize;
+  final bool editable;
+
+  @override
+  State<BrickTextField> createState() => _BrickTextFieldState();
+}
+
+class _BrickTextFieldState extends State<BrickTextField> {
+  void onChange() => widget.onChange(controller.text);
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onChange);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(onChange);
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BrickTextInput(
+      controller: controller,
+      hint: widget.hint,
+      bgColor: widget.bgColor,
+      editable: widget.editable,
+      fontSize: widget.fontSize,
+      maxLines: widget.maxLines,
+      textColor: widget.textColor,
+    );
+  }
+}

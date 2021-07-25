011aeba67f4649609e48e2b5791ae4cc08ac6af8
ltogt <omnesiaorg@gmail.com>
2021-07-25 20:20:34 +0200

    add more grays

--- lib/src/const/brick_colors.dart
+++ lib/src/const/brick_colors.dart
@@ -4,9 +4,13 @@ class BrickColors {
   static const BLACK = Color(0xFF000000);
   static const WHITE = Color(0xFFFFFFFF);
 
+  static const GREY_1 = Color(0xFF111111);
   static const GREY_2 = Color(0xFF222222);
+  static const GREY_2_0 = Color(0xFF202020);
   static const GREY_3 = Color(0xFF333333);
   static const GREY_4 = Color(0xFF444444);
+  static const GREY_5 = Color(0xFF555555);
+  static const GREY_6 = Color(0xFF666666);
 
   static const Color DARK_BLUE = Color(0xFF000011);
 

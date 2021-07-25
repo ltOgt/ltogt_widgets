1b041807a7cd6f0a8ee20ed1f1bb991e125d2098
ltogt <omnesiaorg@gmail.com>
2021-07-25 20:01:35 +0200

    start creating semantic colors

---
+++ lib/src/const/brick_colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/widgets.dart';
+
+class BrickColors {
+  static const BLACK = Color(0xFF000000);
+  static const WHITE = Color(0xFFFFFFFF);
+
+  static const GREY_2 = Color(0xFF222222);
+  static const GREY_3 = Color(0xFF333333);
+  static const GREY_4 = Color(0xFF444444);
+
+  static const Color DARK_BLUE = Color(0xFF000011);
+
+  static const Color GLASS_BLACK = Color(0x22000000);
+
+  static const Color WARNING = Color(0xFFFF3333);
+
+  static const Color background = GREY_4;
+  static const Color background2 = GREY_2;
+
+  static const Color highlightColorLight = Color(0x3DFFFFFF);
+  static const Color highlightColorDark = Color(0x4D000000);
+  static const Color hoverColor = highlightColorLight;
+
+  static const Color borderDark = BLACK;
+  static const Color borderLight = WHITE;
+
+  static const Color shadow = BLACK;
+
+  static const Color buttonIdle = GREY_3;
+  static const Color buttonActive = Color(0xFF335533);
+  static const Color buttonDisabled = background2;
+  static const Color buttonHover = hoverColor;
+  static const Color buttonTextIdle = WHITE;
+  static const Color buttonTextDisabled = WHITE;
+  static const Color buttonTextHover = WHITE;
+}

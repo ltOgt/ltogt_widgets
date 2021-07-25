1672cdf16cad81fb499668ad2bb47af10201c5fa
ltogt <omnesiaorg@gmail.com>
2021-07-25 20:01:19 +0200

    export inner shaddow

--- lib/ltogt_widgets.dart
+++ lib/ltogt_widgets.dart
@@ -7,9 +7,14 @@ export 'src/brick/brick_ink_well.dart';
 export 'src/brick/brick_icon_button.dart';
 export 'src/brick/brick_sortable_list.dart';
 
-export 'src/brick/brick_colors.dart';
+export 'src/const/brick_colors.dart';
 
 export 'src/const/border_radius.dart';
 export 'src/const/padding.dart';
 export 'src/const/sized_box.dart';
 export 'src/const/text_style.dart';
+
+export 'src/inner_shadow/box.dart';
+export 'src/inner_shadow/circle.dart';
+export 'src/inner_shadow/bend_mode.dart';
+export 'src/inner_shadow/bend_container.dart';

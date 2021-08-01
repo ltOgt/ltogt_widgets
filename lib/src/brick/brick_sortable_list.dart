c31349e43a742b46c2df57d995d298c5241c9394
ltogt <omnesiaorg@gmail.com>
2021-08-01 13:59:29 +0200

    clean up > remove constraints > extract shadows > start using theme > extract sort order

--- lib/src/brick/brick_sortable_list.dart
+++ lib/src/brick/brick_sortable_list.dart
@@ -4,6 +4,10 @@ import 'package:ltogt_utils/ltogt_utils.dart';
 import 'package:ltogt_utils_flutter/ltogt_utils_flutter.dart';
 
 import 'package:ltogt_widgets/ltogt_widgets.dart';
+import 'package:ltogt_widgets/src/const/sizes.dart';
+import 'package:ltogt_widgets/src/enum/brick_elevation.dart';
+import 'package:ltogt_widgets/src/enum/sort_order.dart';
+import 'package:ltogt_widgets/src/style/brick_theme_provider.dart';
 
 class SortingOption<T> {
   /// Name of the sorting option
@@ -32,18 +36,16 @@ class ChildData<T> {
 }
 
 class BrickSortableList<T> extends StatefulWidget {
-  static const defaultBoxConstraints = BoxConstraints(maxHeight: 400, maxWidth: 600, minHeight: 200);
-
   const BrickSortableList({
     Key? key,
     required this.childData,
     required this.sortingOptions,
-    this.boxConstraints = defaultBoxConstraints,
     this.sortBarTrailing = const [],
     this.sortBarTrailingClose = const [],
     this.sortBarChildBelow,
-    this.barOnTop = true,
-    this.contentPadding = EdgeInsets.zero,
+    this.isBarOnTop = true,
+    this.additionalContentPadding = EdgeInsets.zero,
+    this.elevation = BrickElevation.RECESSED,
   }) : super(key: key);
 
   /// Items to be build and sorted
@@ -51,9 +53,6 @@ class BrickSortableList<T> extends StatefulWidget {
 
   /// Sorting Options to sort [childData] by
   final List<SortingOption<T>> sortingOptions;
-
-  /// Contstraints for the whole widget
-  final BoxConstraints boxConstraints;
 
   /// Widgets to add to the bar at the end
   final List<Widget> sortBarTrailing;
@@ -66,25 +65,25 @@ class BrickSortableList<T> extends StatefulWidget {
 
   /// If true, the bar is rendered at the start of the list.
   /// Otherwise it is rendered at the bottom
-  final bool barOnTop;
+  final bool isBarOnTop;
 
   /// Additional content padding.
   /// Useful e.g. if you want to overlay additional widgets and push the items accordingly
-  final EdgeInsets contentPadding;
+  final EdgeInsets additionalContentPadding;
+
+  /// Elevation of the widget in relation to the parent widget.
+  ///
+  /// Defaults to [BrickElevation.RECESSED]
+  final BrickElevation elevation;
 
   @override
   State<BrickSortableList> createState() => _BrickSortableListState();
 }
 
-enum _SortOrder {
-  INCR,
-  DECR,
-}
-
 class _BrickSortableListState<T> extends State<BrickSortableList<T>> {
   late SortingOption<T> __sortOption = widget.sortingOptions.first; // TODO handle empty list
-  _SortOrder __sortOrder = _SortOrder.DECR;
-  bool get isOrderDesc => __sortOrder == _SortOrder.DECR;
+  SortOrder __sortOrder = SortOrder.DECR;
+  bool get isOrderDesc => __sortOrder == SortOrder.DECR;
 
   late List<ChildData<T>> __sortedChildren = widget.childData;
 
@@ -110,14 +109,14 @@ class _BrickSortableListState<T> extends State<BrickSortableList<T>> {
   void _order(SortingOption<T> sortOption) {
     __sortedChildren.sort((ChildData<T> p1, ChildData<T> p2) => sortOption._compare<T>(p1.data, p2.data));
 
-    if (__sortOrder == _SortOrder.INCR) {
+    if (__sortOrder == SortOrder.INCR) {
       __sortedChildren = __sortedChildren.reversed.toList();
     }
   }
 
   void changeOrder(SortingOption<T> sortOption) {
     if (sortOption == __sortOption) {
-      __sortOrder = isOrderDesc ? _SortOrder.INCR : _SortOrder.DECR;
+      __sortOrder = isOrderDesc ? SortOrder.INCR : SortOrder.DECR;
       __sortedChildren = __sortedChildren.reversed.toList();
     } else {
       _order(sortOption);
@@ -141,132 +140,80 @@ class _BrickSortableListState<T> extends State<BrickSortableList<T>> {
     });
   }
 
+  EdgeInsetsGeometry _calculateContentPadding() {
+    // : Add padding with same height as bar after first frame (for first frame approx. with 60)
+    final _barPadding = (barPadding ?? 60.0) + 1;
+
+    final basePadding = EdgeInsets.only(
+      top: widget.isBarOnTop ? _barPadding : 0,
+      bottom: widget.isBarOnTop ? 0 : _barPadding,
+      right: 10,
+      left: 10,
+    );
+
+    // : Add additional content padding if user of this widget puts additional elements on top of the list similar to the bar
+    return basePadding.add(widget.additionalContentPadding);
+  }
+
   @override
   Widget build(BuildContext context) {
     if (barPadding == null) {
       calculateBarPaddingInNextFrame();
     }
 
-    return ClipRRect(
-      borderRadius: BORDER_RADIUS_ALL_10,
-      child: Container(
-        decoration: const BoxDecoration(
-          color: BrickColors.GREY_2,
-        ),
-        constraints: widget.boxConstraints,
-        //margin: PADDING_HORIZONTAL_40,
+    final theme = BrickThemeProvider.getTheme(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: theme.radius.medium,
+        color: theme.color.background2,
+        boxShadow: widget.elevation.isElevated ? theme.shadow.elevated : null,
+      ),
+      child: ClipRRect(
+        borderRadius: theme.radius.medium,
         child: Stack(
           children: [
-            Padding(
-              padding: EdgeInsets.only(
-                top: widget.barOnTop ? (barPadding ?? 60.0) + 1 : 0,
-                right: 10,
-                left: 10,
-                bottom: widget.barOnTop ? 0 : (barPadding ?? 60.0) + 1,
-              ).add(widget.contentPadding),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SIZED_BOX_5,
-                    ...ListGenerator.seperated(
-                      seperator: const SizedBox(
-                        width: 2,
-                        height: 2,
-                      ),
-                      list: __sortedChildren,
-                      builder: (ChildData data, int i) => data.build(context),
+            /// ======================================================= LIST
+            Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: theme.radius.medium,
+                  border: Border.all(color: theme.color.borderDark),
+                ),
+                child: Padding(
+                  padding: _calculateContentPadding(),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SIZED_BOX_5,
+                        ...ListGenerator.seperated(
+                          seperator: SIZED_BOX_2,
+                          list: __sortedChildren,
+                          builder: (ChildData data, int i) => data.build(context),
+                        ),
+                        SIZED_BOX_5,
+                      ],
                     ),
-                    SIZED_BOX_5,
-                  ],
+                  ),
                 ),
               ),
             ),
 
-            /// Shadow
-            ...[
-              Positioned(
-                top: 0,
-                right: 0,
-                left: 0,
-                child: Container(
-                  height: 0,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: Offset(0, -2),
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                left: 0,
-                child: Container(
-                  height: 0,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: Offset(0, 2),
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                top: 0,
-                left: 0,
-                child: Container(
-                  width: 0,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: Offset(-2, 0),
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                top: 0,
-                right: 0,
-                child: Container(
-                  width: 0,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: Offset(2, 0),
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            /// ======================================================= RECESS SHADOWS
+            if (widget.elevation.isRecessed) ...[
+              /// ----------------------------------------------------- Top
+              const _RecessShadow(alignment: Alignment.topCenter),
+              const _RecessShadow(alignment: Alignment.centerLeft),
+              const _RecessShadow(alignment: Alignment.bottomCenter),
+              const _RecessShadow(alignment: Alignment.centerRight),
             ],
-            // Bar
+
+            /// ======================================================= ORDER BAR
             Positioned(
-              top: widget.barOnTop ? 0 : null,
-              bottom: widget.barOnTop ? null : 0,
+              top: widget.isBarOnTop ? 0 : null,
+              bottom: widget.isBarOnTop ? null : 0,
               left: 0,
               right: 0,
               child: _OrderBar<T>(
@@ -280,6 +227,88 @@ class _BrickSortableListState<T> extends State<BrickSortableList<T>> {
                 trailingClose: widget.sortBarTrailingClose,
                 childBelow: widget.sortBarChildBelow,
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _RecessShadow extends StatelessWidget {
+  const _RecessShadow({
+    Key? key,
+    required this.alignment,
+  }) : super(key: key);
+
+  final Alignment alignment;
+  void assertAlignment() {
+    assert(
+      [
+        Alignment.centerLeft,
+        Alignment.centerRight,
+        Alignment.topCenter,
+        Alignment.bottomCenter,
+      ].contains(alignment),
+      "Unsupported alignment: $alignment",
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assertAlignment();
+
+    final theme = BrickThemeProvider.getTheme(context);
+
+    double? top, left, bottom, right, height, width;
+    Offset offset = Offset.zero;
+    if (alignment == Alignment.topCenter) {
+      top = 0;
+      right = 0;
+      left = 0;
+      height = 0;
+      offset = const Offset(0, -2);
+    }
+    //
+    else if (alignment == Alignment.centerLeft) {
+      bottom = 0;
+      top = 0;
+      left = 0;
+      width = 0;
+      offset = const Offset(-2, 0);
+    }
+    //
+    else if (alignment == Alignment.centerRight) {
+      bottom = 0;
+      top = 0;
+      right = 0;
+      width = 0;
+      offset = const Offset(2, 0);
+    }
+    //
+    else if (alignment == Alignment.bottomCenter) {
+      bottom = 0;
+      right = 0;
+      left = 0;
+      height = 0;
+      offset = const Offset(0, 2);
+    }
+
+    return Positioned(
+      top: top,
+      right: right,
+      left: left,
+      bottom: bottom,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: offset,
+              color: theme.color.shadow,
             ),
           ],
         ),
@@ -312,17 +341,22 @@ class _OrderBar<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = BrickThemeProvider.getTheme(context);
+
+    final _arrowDownIcon = Icon(Icons.arrow_downward, color: theme.color.icon);
+    final _arrowUpIcon = Icon(Icons.arrow_upward, color: theme.color.icon);
+
     return Container(
       decoration: BoxDecoration(
-        color: BrickColors.GREY_2,
-        borderRadius: BORDER_RADIUS_ALL_10,
-        border: Border.all(color: Colors.black),
-        boxShadow: const [
+        color: theme.color.background2,
+        borderRadius: theme.radius.medium,
+        border: Border.all(color: theme.color.borderDark),
+        boxShadow: [
           BoxShadow(
-            blurRadius: 2,
-            spreadRadius: -2,
-            color: Colors.black,
-            offset: Offset(0, 4),
+            blurRadius: 4,
+            spreadRadius: 2,
+            color: theme.color.shadow,
+            offset: const Offset(0, -2),
           ),
         ],
       ),
@@ -344,9 +378,9 @@ class _OrderBar<T> extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             BrickIconButton(
-              size: 30,
+              size: SMALL_BUTTON_SIZE,
               onPressed: (_) => onToggleDirection(),
-              icon: isOrderDesc ? const Icon(Icons.arrow_downward) : const Icon(Icons.arrow_upward),
+              icon: isOrderDesc ? _arrowDownIcon : _arrowUpIcon,
             ),
             ...ListGenerator.seperated(
               seperator: SIZED_BOX_10,
@@ -355,7 +389,7 @@ class _OrderBar<T> extends StatelessWidget {
               builder: (SortingOption<T> option, int i) {
                 return BrickButton(
                   mode: (sortOption == option) ? BendMode.CONCAVE : BendMode.CONVEX,
-                  bgColor: (sortOption == option) ? BrickColors.buttonActive : BrickColors.buttonIdle,
+                  bgColor: (sortOption == option) ? theme.color.buttonActive : theme.color.buttonIdle,
                   onPress: () => onChangeOrder(
                     option,
                   ),
